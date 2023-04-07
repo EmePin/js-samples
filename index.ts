264d02b219c6e724fdb58d9ac313ee010443a421
Aimee Pineda <aimeejahdai@gmail.com>
2023-04-06 22:16:06 -0600

    funciona

--- index.ts
+++ index.ts
@@ -4,15 +4,35 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-let map: google.maps.Map;
+// Initialize and add the map
+let map;
 async function initMap(): Promise<void> {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+
+  // Request needed libraries.
   //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  map = new Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+
+  // The map, centered at Uluru
+  map = new Map(
+    document.getElementById('map') as HTMLElement,
+    {
+      zoom: 4,
+      center: position,
+      mapId: 'DEMO_MAP_ID',
+    }
+  );
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: 'Uluru'
   });
 }
 
 initMap();
-export {};
+
+export { };
